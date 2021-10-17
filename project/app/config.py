be87a782c2e44ae730b0b18af97c220ed2ad227b
Rudra Raina <rudraina@Rudras-MacBook-Air.local>
2021-10-17 13:09:01 +0530

    setting postgres database

--- project/app/config.py
+++ project/app/config.py
@@ -3,12 +3,14 @@ import os
 from functools import lru_cache
 
 from pydantic import BaseSettings
+from pydantic.networks import AnyUrl
 
 log = logging.getLogger('uvicorn')
 
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool= os.getenv("TESTING",0)
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 @lru_cache()
 def get_settings() -> BaseSettings:
